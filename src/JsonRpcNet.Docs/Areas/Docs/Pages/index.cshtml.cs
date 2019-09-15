5b6d770252497ddb5c3a3d44e996e93655defb0d
Fabricio Bastian <fabriciobastian@gmail.com>
2019-09-15 19:21:27 +0200

    Add initial version of docs razor pages project (/docs - static content so far) with support for embedded resources

---
+++ src/JsonRpcNet.Docs/Areas/Docs/Pages/index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JsonRpcNet.Docs.MyFeature.Pages
+{
+    public class JsonRpcDocsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/JsonRpcNet.Docs/JsonRpcNetDocsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonRpcNet.Docs
+{
+    public static class JsonRpcNetDocsExtensions
+    {
+        public static void AddJsonRpcNetDocs(this IServiceCollection services)
+        {
+            services.ConfigureOptions(typeof(JsonRpcNetDocsConfigureOptions));
+        }
+    }
+}
