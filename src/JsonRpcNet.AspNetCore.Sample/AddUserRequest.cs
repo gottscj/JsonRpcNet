8fb9da2ecf59eaebe56cfd4401ce5a10b420976f
Jonas Gottschau <jonas@gottschau.dk>
2019-11-03 20:58:55 +0100

    Update AddUserRequest.cs

--- src/JsonRpcNet.AspNetCore.Sample/AddUserRequest.cs
+++ src/JsonRpcNet.AspNetCore.Sample/AddUserRequest.cs
@@ -2,7 +2,7 @@
 
 namespace JsonRpcNet.AspNetCore.Sample
 {
-    public abstract class AddUserRequest
+    public class AddUserRequest
     {
         public string Id { get; set; }
         public DateTime TimeStamp { get; set; }
