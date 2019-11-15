5080a9fe303534a665c7161fe0b1d2633576b77d
Fabricio Bastian <fabriciobastian@gmail.com>
2019-11-15 02:07:21 +0100

    Add very simple styling to the notification panel

--- src/JsonRpcNet.Docs/web/src/components/NotificationPanel.vue
+++ src/JsonRpcNet.Docs/web/src/components/NotificationPanel.vue
@@ -1,9 +1,15 @@
 <template>
-  <ul>
-    <div v-for="(note, index) in notifications" v-bind:key="`note-${index}`">
-      [{{ note.timestampStr() }}] {{ note.title }}
+  <div id="NotificationPanel">
+    <div class="panel">
+      <div
+        class="panel-notification"
+        v-for="(note, index) in notifications"
+        v-bind:key="`note-${index}`"
+      >
+        [{{ note.timestampStr() }}] {{ note.title }}
+      </div>
     </div>
-  </ul>
+  </div>
 </template>
 
 <script>
@@ -15,4 +21,28 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#NotificationPanel {
+  .panel {
+    margin-left: 4px;
+    margin-right: 4px;
+  }
+
+  .panel-notification {
+    background-color: map-get($accent-color, 30);
+    color: inherit;
+    cursor: pointer;
+    padding: 5px;
+    width: 100%;
+    margin-top: 4px;
+    margin-bottom: 4px;
+    text-align: left;
+    outline: none;
+    font-size: 12px;
+    border-style: solid;
+    border-width: 1px;
+    border-radius: 5px 5px 5px 5px;
+    border-color: map-get($accent-color, 400);
+  }
+}
+</style>
