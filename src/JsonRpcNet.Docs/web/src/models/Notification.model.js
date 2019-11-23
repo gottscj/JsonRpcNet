6f66a01acac750a588c980da1b60c71f0a66db8b
Fabricio Bastian <fabriciobastian@gmail.com>
2019-11-23 21:53:10 +0100

    Change notification timestamp date format

--- src/JsonRpcNet.Docs/web/src/models/Notification.model.js
+++ src/JsonRpcNet.Docs/web/src/models/Notification.model.js
@@ -12,8 +12,8 @@ export class Notification {
     const d = this.timestamp;
 
     /* eslint-disable */
-    return `${(d.getMonth() + 1).toString().padStart(2, '0')}/${
-      d.getDate().toString().padStart(2, '0')}/${
+    return `${d.getDate().toString().padStart(2, '0')}/${
+      (d.getMonth() + 1).toString().padStart(2, '0')}/${
       d.getFullYear().toString().padStart(4, '0')} ${
       d.getHours().toString().padStart(2, '0')}:${
       d.getMinutes().toString().padStart(2, '0')}:${
