0305d5e66c68af6a06273aba951b6886e6e3a889
Jonas Gottschau <Gottschau_Jonas@shure.com>
2019-11-27 16:26:39 +0100

    Added support for $ref of enum without type property

--- src/JsonRpcNet.Docs/web/src/services/TypeDefinitions.service.js
+++ src/JsonRpcNet.Docs/web/src/services/TypeDefinitions.service.js
@@ -63,7 +63,9 @@ export class TypeDefinitionsService {
     if (this.isReferenceType(type)) {
       paramTypeDef = this.getTypeReferenceDefinition(type);
     }
-
+    if(paramTypeDef.type === undefined && ("enum" in paramTypeDef)) {
+      return paramTypeDef.enum[0];
+    }
     switch (paramTypeDef.type.toLowerCase()) {
       case "object": {
         let paramType = {};
